4f35e9a1bdc2184be29d60c05eda28d1ddd14bc2
Adarsh Dhanware <adarshdhanware@gmail.com>
2025-07-04 17:01:56 +0530

    Some Updates

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({extended:true,limit:"10mb"}))
 app.use(cookieParser());
 app.use(cors(
     {
-        origin:"http://localhost:5173",
+        origin:process.env.CORS_ORIGIN,
         credentials:true
     }
 ))
@@ -29,13 +29,13 @@ app.use(cors(
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+// if(process.env.NODE_ENV==="production"){
+//     app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-    })
-}
+//     app.get("*",(req,res)=>{
+//         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+//     })
+// }
 
 server.listen(PORT,()=>{
     console.log("Server is running  on PORT " + PORT);
